8d51e9ba474d73ca8f76711b2400489b439c67c8
WojciechPietruszynski <80004720+WojciechPietruszynski@users.noreply.github.com>
2021-03-07 21:26:30 +0100

    Lab2 part time of work

--- Lab2/Lab2_PDF/teststudent.py
+++ Lab2/Lab2_PDF/teststudent.py
@@ -1,9 +1,14 @@
-import student
+import Lab2.studencik as stud
 
 
 def testy():
-    student1 = student()
+    student = stud.Studencik('Wojciech', 'Pietruszyński', 19443, 'Informatyka')
+    print(student.getLicznik(), student)
+    student1 = stud.StudentInformatyki('Jan', 'Kowalski', 12345, None, 'Programowanie')
+    print(student1.getLicznik(), student1)
+    student2 = stud.StudentInformatyki('Jan', 'Nowak', 12346, None, 'Programowanie')
+    print(student2.getLicznik(), student2)
 
 
-if __name == "__main__":
+if __name__ == "__main__":
     testy()
---
+++ Lab2/studencik.py
@@ -0,0 +1,28 @@
+class Studencik:
+    licznik = 0
+
+    def __init__(self, imie, nazwisko, __numer_indeksu, kierunek):
+        Studencik.licznik += 1
+        self.imie = imie
+        self.nazwisko = nazwisko
+        self.numer_indeksu = __numer_indeksu
+        self.kierunek = kierunek
+
+    def __str__(self):
+        return f'Imię : {self.imie}, Nazwisko :  {self.nazwisko}, Numer indeksu : {self.numer_indeksu}, ' \
+               f'Kierunek : {self.kierunek} '
+
+    def getLicznik(self):
+        return Studencik.licznik
+
+
+class StudentInformatyki(Studencik):
+
+    def __init__(self, imie, nazwisko, __numer_indeksu, kierunek, specjalnosc):
+        super().__init__(imie, nazwisko, __numer_indeksu, kierunek)
+        self.kierunek = 'IIS'
+        self.specjalnosc = specjalnosc
+
+    def __str__(self):
+        return f'Imię : {self.imie}, Nazwisko :  {self.nazwisko}, Numer indeksu : {self.numer_indeksu}, ' \
+               f'Kierunek : {self.kierunek} Specjalnośc : {self.specjalnosc} '
