7563a35f26def7ead6ac49cf08f8d8cb3c4d5e0c
WojciechPietruszynski <80004720+WojciechPietruszynski@users.noreply.github.com>
2021-03-07 22:47:30 +0100

    Lab2 zrealizowane zadanie 1,2,3,4,5

--- Lab2/Student.py
+++ Lab2/Student.py
@@ -1,28 +1,46 @@
 class Student:
     licznik = 0
 
-    def __init__(self, imie, nazwisko, __numer_indeksu, kierunek):
+    def __init__(self, imie, nazwisko, numerIndeksu, kierunek):
         Student.licznik += 1
         self.imie = imie
         self.nazwisko = nazwisko
-        self.numer_indeksu = __numer_indeksu
+        self.__numerIndeksu = numerIndeksu
         self.kierunek = kierunek
 
     def __str__(self):
-        return f'Imię : {self.imie}, Nazwisko :  {self.nazwisko}, Numer indeksu : {self.numer_indeksu}, ' \
-               f'Kierunek : {self.kierunek} '
+        return "Imię: %s Nazwisko: %s Kierunek: %s Indeks: %s" % (
+            self.imie, self.nazwisko, self.kierunek, self.__numerIndeksu)
+
+    def __eq__(self, other):
+        if self.imie == other.imie and self.nazwisko == other.nazwisko:
+            return True
+        else:
+            return False
+
+    def __lt__(self, other):
+        if self.imie < other.imie and self.nazwisko < other.nazwisko:
+            return True
+        else:
+            return False
 
     def getLicznik(self):
         return Student.licznik
 
+    def rokDrugi(self):
+        print(self.__str__() + ' Rok: 2')
+
+    ## Usprawniona fukcja rok_drugi według mnie powinna przyjmować dodatkowy parametr
+    # w tym wypoadku rok studiów
+    # poprawiona funcja prezentuje się następująco
+    # def rok_studiow(self, rokStudiow):
+    #     print(self.__str__() + ' Rok:', rokStudiow)
+    # funkcja rok drugi nie jest zła jednak, takie harcodowanie nie jest dobra praktyka
+    # starajmy sie aby nasze rozwiązania były reużywalne oraz miały sens
 
 class StudentInformatyki(Student):
 
-    def __init__(self, imie, nazwisko, __numer_indeksu, kierunek, specjalnosc):
-        super().__init__(imie, nazwisko, __numer_indeksu, kierunek)
+    def __init__(self, imie, nazwisko, numerIndeksu, kierunek, specjalnosc):
+        super().__init__(imie, nazwisko, numerIndeksu, kierunek)
         self.kierunek = 'IIS'
         self.specjalnosc = specjalnosc
-
-    def __str__(self):
-        return f'Imię : {self.imie}, Nazwisko :  {self.nazwisko}, Numer indeksu : {self.numer_indeksu}, ' \
-               f'Kierunek : {self.kierunek} Specjalnośc : {self.specjalnosc} '
--- Lab2/teststudent.py
+++ Lab2/teststudent.py
@@ -2,12 +2,30 @@ import Lab2.Student as stud
 
 
 def testy():
-    student = stud.Student('Wojciech', 'Pietruszyński', 19443, 'Informatyka')
-    print(student.getLicznik(), student)
-    student1 = stud.StudentInformatyki('Jan', 'Kowalski', 12345, None, 'Programowanie')
-    print(student1.getLicznik(), student1)
-    student2 = stud.StudentInformatyki('Jan', 'Nowak', 12346, None, 'Programowanie')
-    print(student2.getLicznik(), student2)
+    print('Zadanie 1,Zadanie 3 ,Zadanie 4')
+    s1 = stud.Student('Wojciech', 'Pietruszyński', 19443, 'Informatyka')
+    # print(student.numer_indeksu) jeśli pole prywatne to nie da rady go wydrukowac brak dostępu
+    print(s1.getLicznik(), s1)
+    s2 = stud.StudentInformatyki('Jan', 'Kowalski', 12345, None, 'Programowanie')
+    print(s2.getLicznik(), s2.__str__(), 'Specjalnosc: ', s2.specjalnosc)
+    s3 = stud.StudentInformatyki('Jan', 'Nowak', 12346, None, 'Programowanie')
+    print(s3.getLicznik(), s3.__str__(), 'Specjalnosc: ', s3.specjalnosc)
+    print('Zadanie 2')
+    print(s1.imie, s1.nazwisko, "<", s2.imie, s2.nazwisko)
+    print(s1.__lt__(s2))
+    print(s1.imie, s1.nazwisko, "<", s3.imie, s3.nazwisko)
+    print(s2.__lt__(s1))
+    print()
+    print(s1.imie, s1.nazwisko, "==", s2.imie, s2.nazwisko)
+    print(s1.__eq__(s2))
+    print(s1.imie, s1.nazwisko, "==", s3.imie, s3.nazwisko)
+    print(s2.__eq__(s1))
+    print(s1.imie, " ", s1.nazwisko, "==", s1.imie, s1.nazwisko)
+    print(s1.__eq__(s1))
+
+    print("Zadanie 5")
+    stud.Student.rokDrugi(s1,3)
+
 
 
 if __name__ == "__main__":
